4662272eb23ef52e32e7390220dc672690edcd17
sparrow1488 <ilyaokubev@gmail.com>
2020-08-09 20:17:27 +0400

    все обновил кароче

---
+++ Всякая шняга/Обработка события/app.js
@@ -0,0 +1,13 @@
+function countStart(){
+    let count = 0
+    return function(){
+        count++
+        console.log(count)
+        if (count % 10 == 0){
+            console.log('Аче')
+        }
+    }
+}
+console.log('я хочу пиццу')
+const click = countStart()
+document.onclick = click
