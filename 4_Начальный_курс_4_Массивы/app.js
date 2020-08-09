a8adcd5ce75c1a011aea9326334e5fb05121a5fc
sparrow1488 <ilyaokubev@gmail.com>
2020-08-09 21:09:43 +0400

    начал циклы

--- 4_Начальный_курс_4_Массивы/app.js
+++ 4_Начальный_курс_4_Массивы/app.js
@@ -1,1 +1,10 @@
-console.log('проверка')
+//9 МАССИВЫ
+const cars = ['Вольво', 'Мазда', 'Форд'] //правильное
+console.log(cars)
+// const carsNew = new Array('Вольво', 'Мазда', 'Форд') //полное расписывание
+// console.log(carsNew)
+console.log(cars.length) // длинна массива
+cars[0] = 'Феррари' //замена эллемента массива
+console.log(cars)
+cars[cars.length] = 'новая машына'
+console.log(cars)
