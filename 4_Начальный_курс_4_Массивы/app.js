93029f320c96a965242c27e855d8ab4f05c97930
sparrow1488 <ilyaokubev@gmail.com>
2020-08-09 21:22:05 +0400

    закончил цикл for

--- 4_Начальный_курс_4_Массивы/app.js
+++ 4_Начальный_курс_4_Массивы/app.js
@@ -8,3 +8,17 @@ cars[0] = 'Феррари' //замена эллемента массива
 console.log(cars)
 cars[cars.length] = 'новая машына'
 console.log(cars)
+
+//10 ЦИКЛЫ
+
+for (let i = 0; i < cars.length; i++) {
+    console.log(cars[i])
+}
+
+console.log('')
+console.log('способ for of')
+console.log('')
+
+for (let car of cars) {
+    console.log(car)
+}
