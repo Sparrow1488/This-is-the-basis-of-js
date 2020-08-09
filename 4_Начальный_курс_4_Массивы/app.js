e66e3cc7f80bb31bec5daf11c13bfeee5281dceb
sparrow1488 <ilyaokubev@gmail.com>
2020-08-09 23:06:26 +0400

    балуюсь с объектами

--- 4_Начальный_курс_4_Массивы/app.js
+++ 4_Начальный_курс_4_Массивы/app.js
@@ -22,3 +22,38 @@ console.log('')
 for (let car of cars) {
     console.log(car)
 }
+
+// ЗАДАЧА, НО НЕ ВКЛЮЧАЙ ОНА ДАЕТ БЕСКОНЕЧНЫЙ ЦИКЛ
+// const arr = [1, 2 ,3]
+
+// for (let i = 0; i = 6; i++) {
+//     let b = 0
+//     // (arr[b] + arr[b + 1])
+//     console.log((arr[b] + arr[b + 1]))
+// }
+//------------------------------
+
+
+//11 ОБЪЕКТЫ
+
+const person = {
+    firstName: 'Valentin',
+    lastName: 'Sparrow',
+    birthYear: 2003,
+    agePerson: function calculateAge(year) {
+        return 2020 - person.birthYear
+    }, 
+    hobby: ['sigaretes', 'vgames', 'programming'],
+    aboutPerson: 'Типо описание',
+    haveSex: false,
+    greet: function() {
+        console.log('Ого! ОПИСАНИЕ АХАХАХ. Чекай че могу')
+        console.log(3 + this.agePerson())
+    }
+}
+console.log('Имя человека: ' + person.firstName)
+person.greet()
+//мдээ, такая вот шиза
+console.log(person['hobby'])
+console.log(person.hobby[1])
+
