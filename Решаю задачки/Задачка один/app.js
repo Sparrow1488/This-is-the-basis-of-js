2d707c77a0fd5e534cf14ed9173ef97fbe0f1053
unknown <ilysokubev@gmail.com>
2020-08-11 16:41:23 +0300

    исправил небольшой баг

--- Решаю задачки/Задачка один/app.js
+++ Решаю задачки/Задачка один/app.js
@@ -3,7 +3,7 @@
 
 console.log('Из какого диапозона необходимо вывести числа?')
 let minNum = 0 //с какого числа начать отсчет
-let maxNum = 300 // до какого числа выполнять условие?
+let maxNum = 150 // до какого числа выполнять условие?
 
 let cal = 0 // счетчик чисел, которое выявила машина по условию
 let sum = 0 //счетчик сумм предыдущего числа с последующим
@@ -19,7 +19,7 @@ for (let i = minNum; i <= maxNum; i++) {
 }
 console.log('')
 console.log('Операция окончена')
-// let result = `Программа выявила: ${cal.toString(10).endsWith('1' || '2' || '3' || '4') ? cal + ' совпадения' : cal + ' совпадений' } с поставленным условием`
+//  let result = `Программа выявила: ${cal.toString(10).endsWith('1' || '2' || '3' || '4') ? cal + ' совпадения' : cal + ' совпадений' } с поставленным условием`
 let result = `Программа выявила: ${cal.toString(10).endsWith('1') || cal.toString(10).endsWith('2') || cal.toString(10).endsWith('3') || cal.toString(10).endsWith('4') ? cal + ' совпадения' : cal + ' совпадений' } с поставленным условием`
 console.log(result) //вывожу результат
 console.log('Сумма = ' + sum) //вывожу сумму
@@ -40,14 +40,14 @@ console.log('Сумма = ' + sum) //вывожу сумму
 // 10..toString(2)
 // console.log(b, typeof b)
 // console.log(typeof b)
-let i = 54
-console.log(i.toString(10).indexOf('4'))
+// let i = 54
+// console.log(i.toString(10).indexOf('4'))
 
-for (let i = 1; i <= 100; i++) {
-    if (i.toString(10).endsWith('4')) {
-        console.log(i + ' имеет 4')
-        // console.log(typeof i)
-    } else {
-        console.log(i + ' не имеет 4')
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     if (i.toString(10).endsWith('4')) {
+//         console.log(i + ' имеет 4')
+//         // console.log(typeof i)
+//     } else {
+//         console.log(i + ' не имеет 4')
+//     }
+// }
