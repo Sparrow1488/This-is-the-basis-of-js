7f15acfb866b51dc35664d3a7ccd579f2e50ce53
sparrow1488 <ilyaokubev@gmail.com>
2020-08-13 23:59:06 +0400

    остановился на стрелочных функциях

--- 7_Не_начальный_курс_3_функции/app.js
+++ 7_Не_начальный_курс_3_функции/app.js
@@ -1,12 +1,12 @@
-//ФУНКЦИИ
+// 1 ФУНКЦИИ
 //Function Declaration
 function greet(name) {
     console.log('Здарова, ' + name)
 }
 //Function Expression
-const greet2 = function(name) { 
+const greet2 = function greetFunction(name) { 
     console.log('Здарова, ' + name)
-} //это анонимная функция, тк нет имени у функции
+} 
 greet('борбосы')
 greet2('бибисы')
 
@@ -14,5 +14,24 @@ greet2('бибисы')
 //еще к первой функции можно обратиться когда угодно. Хоть объявить ее до создания самой функции,
 //в то время как к Expression нельзя так обратиться.
 
-console.log(typeof greet)
-console.dir(greet)
+// console.log(typeof greet) //просмотр типа 
+// console.dir(greet) //более детальный просмотр (через браузер)
+
+// 2 АНОНИМНЫЕ ФУНКЦИИ
+let counter = 0
+const interval = setInterval(function(){
+    if (counter === 5) {
+        console.log('шутка')
+        clearInterval(interval)
+    } else {
+        console.log(++counter)
+    }
+}, 1000) //интервал через который выполняется функция (function, time)
+
+// 3 СТРЕЛОЧНЫЕ ФУНКЦИИ
+
+const arrow = (name) => {
+    console.log('Здарова, ' + name)
+}
+
+arrow('пес')
