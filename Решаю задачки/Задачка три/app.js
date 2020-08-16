43b004104bad3d490d567fbc9de59bb27dcd47e8
sparrow1488 <ilyaokubev@gmail.com>
2020-08-16 21:44:22 +0400

    задача 3 с массивами

---
+++ Решаю задачки/Задачка три/app.js
@@ -0,0 +1,26 @@
+// НАПИСАЛ КОНСОЛЬКУ, КОТОРАЯ ИЩЕТ В МАССИВЕ ИМЯ ЧЕЛОВЕКА И ЕСЛИ НЕ НАХОДИТ, ТО ДОБАВЛЯЕТ ЕГО
+// В БАЗУ
+const people = [
+    {name: 'Игорь', money: 2000, male: 'man'},
+    {name: 'Юра', money: 1000, male: 'man'},
+    {name: 'Женя', money: 5000, male: 'man'},
+    {name: 'Кира', money: 6000, male: 'girl'},
+    {name: 'Саня', money: 20000, male: 'girl'},
+    {name: 'Саня', money: 25000, male: 'man'},
+    {name: 'Вика', money: 2000, male: 'girl'}
+]
+
+let namePeople = 'Юра2'
+let moneyPeople = 30000
+let malePeople = 'man'
+let index = people.findIndex(function(people){
+        return people.name == namePeople
+})
+if (index === -1){
+    people.push({name: namePeople, money: moneyPeople, male: malePeople})
+    index = people.findIndex(function(people){
+        return people.name == namePeople
+}) 
+} 
+console.log(index)
+console.log(people)
