9bc9b54fad6c557bfec3defb57aeb16012ddaa90
sparrow1488 <ilyaokubev@gmail.com>
2020-08-16 12:03:34 +0400

    задача 2

---
+++ Решаю задачки/Задачка два/app.js
@@ -0,0 +1,16 @@
+// function bazaMembersVip(name = "") {
+//     const arrMembers = ['Пип', 'Учин']
+//     let addName = arrMembers.push(this.name)
+//     console.log('Добавлен юзер ' + addName)
+//     console.log('База: ' + arrMembers)
+    
+// }
+
+// function pushFun(addName){
+//     console.log('Добавлен новый юзер: ' + addName)
+//     // let pushArr = arrMembers.push(addName)
+//     // console.log('База обновлена: ' + pushArr)
+// }
+
+// pushFun('Хуеплет')
+// bazaMembersVip()
---
+++ Решаю задачки/Задачка два/app2.js
@@ -0,0 +1,22 @@
+const arrMembers = ['Admin1']
+
+function addMember(name = '') {
+    let addMem = arrMembers.push(name)
+    console.log('Добавлен новый пользователь: ' + name)
+}
+
+function showInfoMembers(){
+    console.log(arrMembers)
+}
+
+function calculateMembers(cal = 0){
+    cal = arrMembers.length
+    console.log('Пользователей всего: ' + cal)
+}
+
+addMember('Степа')
+addMember('Гена')
+addMember('Арнольд')
+addMember('Мужик')
+showInfoMembers()
+calculateMembers()
