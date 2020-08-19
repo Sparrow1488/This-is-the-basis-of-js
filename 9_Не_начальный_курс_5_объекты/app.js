b34db5e8f9ecd552c606bd99d41e5a53fe32c31c
Sparrow1488 <ilyaokubev@gmail.com>
2020-08-19 09:54:10 +0300

    закончил хуиту

--- 9_Не_начальный_курс_5_объекты/app.js
+++ 9_Не_начальный_курс_5_объекты/app.js
@@ -1,24 +1,28 @@
 // ОБЪЕКТЫ
 const person = {
     name: 'Валентин',
-    age: 26,
+    age: undefined,
     languages: ['ru', 'en'],
     isProgrammer: true,
     ['key_' + (1 + 4)]: 'return key 4',
     'complex key': 'complex value',
-    greet1: function() {
-        console.log('greet1 описание')
-    },
-    greet2() {
-        console.log('greet2 описание')
+    // greet1: function() {
+    //     console.log('greet1 описание')
+    // },
+    // greet2() {
+    //     console.log('greet2 описание')
+    // }
+    info() {
+        console.log('this: ', this)
+        console.log('Информация про человека по имени: ' + this.name)
     }
 }
 
 console.log(person.name)
 console.log(person['age'])
 console.log(person['complex key'])
-person.greet1()
-person.greet2()
+// person.greet1()
+// person.greet2()
 
 person.age += 4
 person.languages.push('by')
@@ -34,6 +38,7 @@ const {name, age: personAge = 4, languages} = person
 console.log(name, personAge, languages)
 // console.log(person)
 
+console.log('1 СПОСОБ ПЕРЕБОРА')
 //1 СПОСОБ ПЕРЕБОРА - не очень
 //безопасный перебор объекта через условие (опасность в случайном захождение в property устранена)
 for (key in person){
@@ -42,13 +47,32 @@ for (key in person){
     }
 }
 
+console.log('2 СПОСОБ ПЕРЕБОРА')
 //2 СПОСОБ ПЕРЕБОРА - норм
 const keys = Object.keys(person) //получим массив из ключей
 keys.forEach((key) => {
     console.log('key:' + key, 'value:' + person[key])
 })
+console.log('2.1 СПОСОБ ПЕРЕБОРА')
+// 2.1 СПОСОБ ПЕРЕБОРА
+Object.keys(person).forEach(key => console.log('key:' + key, 'value:' + person[key]))
 
-//2.1 СПОСОБ ПЕРЕБОРА
-// Object.keys(person).forEach((key) => {
-//     console.log('key:' + key, 'value:' + person[key])
-// })
+//Context
+person['key_5'] = undefined
+person.info()
+
+const logger = {
+    keys(obj) {
+        console.log('Obj keys:', Object.keys(obj))
+    },
+
+    keysAndValues(obj) {
+        console.log('Obj keys:', Object.keys(obj))
+        const keys = Object.keys(obj)
+        keys.forEach(key => {
+            console.log('key:' + key, 'value:' + person[key])
+        })
+    }
+}
+logger.keys(person)
+logger.keysAndValues(person)
