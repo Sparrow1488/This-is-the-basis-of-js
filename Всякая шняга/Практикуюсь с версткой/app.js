b4495609829036f7f13890d91e33952c94d83699
Sparrow1488 <ilyaokubev@gmail.com>
2020-09-11 19:57:22 +0300

    я выпил всю верстку

--- Всякая шняга/Практикуюсь с версткой/app.js
+++ Всякая шняга/Практикуюсь с версткой/app.js
@@ -1,24 +1,24 @@
 const mainBtn = document.querySelector('.header-btns-main-btn');
 const test = document.querySelector('.test')
 const mainContent = document.querySelector('.main-block')
-function addF(){
-    const testBody = document.createElement('div')
-    modal.classList.add('cardTest')
-    modal.insertAdjacentHTML('afterbegin', `
-    <div class="card">
-    <h3  class="title-card">Крутой титул</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolore, voluptates eum, nesciunt consequatur error culpa quidem commodi distinctio voluptas nihil fugiat numquam omnis soluta laborum ex delectus. Labore autem doloribus iste aperiam dicta quod nisi rem maiores tempora sit?</p>
-    <div class="card-img">
-        <img src="img-test-card.jpg" alt="Крутая фотка">
-    </div>
-    <div class="foot-card">
-        <button>Узнать больше</button>
-    </div>
-</div>
-    `)
-}
+// function addF(){
+//     const testBody = document.createElement('div')
+//     modal.classList.add('cardTest')
+//     modal.insertAdjacentHTML('afterbegin', `
+//     <div class="card">
+//     <h3  class="title-card">Крутой титул</h3>
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolore, voluptates eum, nesciunt consequatur error culpa quidem commodi distinctio voluptas nihil fugiat numquam omnis soluta laborum ex delectus. Labore autem doloribus iste aperiam dicta quod nisi rem maiores tempora sit?</p>
+//     <div class="card-img">
+//         <img src="img-test-card.jpg" alt="Крутая фотка">
+//     </div>
+//     <div class="foot-card">
+//         <button>Узнать больше</button>
+//     </div>
+// </div>
+//     `)
+// }
 
 mainBtn.addEventListener('click', function(e){
-    testBody.addF()
+    // testBody.addF()
     console.log('ok')
 })
